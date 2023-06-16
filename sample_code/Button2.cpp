aa0a9c4df9b9c87f6606f16c2e3dfd59e2efc7c1
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-06-16 16:22:41 +0900

    Button2更新

--- sample_code/Button2.cpp
+++ sample_code/Button2.cpp
@@ -6,110 +6,135 @@
 
 #include <M5Unified.h>
 
-  /// この記述で、以後 "M5.Display" の代わりに "dsp" と書くことができるようになる。
-auto &dsp = M5.Display;
-
-  /// この記述で、以後 "M5.BtnA" の代わりに "btn" と書くことができるようになる。
-  /// 操作対象のボタンを変更したい場合はここの記述を変更する。
-auto &btn = M5.BtnA;
-
-  /// CoreInk,Core2,CoreS3,Tough,StickC/CPlus の場合は M5.BtnPWR で電源ボタンも使用できる。
-// auto &btn = M5.BtnPWR;
-  /// ※ 但しCoreInk以外の機種の BtnPWR については、電源管理ICがボタンを管理しており、
-  ///    今ボタンが押された状態かどうかは判定ができない。
-  ///    使える機能は wasClick / wasHold および連続クリック回数に関する機能のみに限定される。
-
-  /// この記述で "M5.Speaker" の代わりに "spk" と書くことができるようになる。
-auto &spk = M5.Speaker;
-
 void setup(void)
 {
-  /// 設定用の構造体を取得。
-  auto cfg = M5.config();
-//cfg.external_speaker.hat_spk = true;    /// HAT SPK  を使う場合、これを追加する。
-//cfg.external_speaker.hat_spk2 = true;   /// HAT SPK2 を使う場合、これを追加する。
-//cfg.external_speaker.atomic_spk = true; /// ATOM SPK を使う場合、これを追加する。
-
   /// M5Unifiedを使用する準備をする。
-  M5.begin(cfg);
+  M5.begin();
 
   /// 電子ペーパの場合は描画速度が最も速いモードに変更する。
-  dsp.setEpdMode(m5gfx::epd_fastest);
+  M5.Display.setEpdMode(m5gfx::epd_fastest);
 
   /// 画面が縦長の場合は横長配置に変更する。
-  if (dsp.width() < dsp.height())
+  if (M5.Display.width() < M5.Display.height())
   {
-    dsp.setRotation(dsp.getRotation() ^ 1);
+    M5.Display.setRotation(M5.Display.getRotation() ^ 1);
   }
+
+  M5.Display.setFont(&fonts::AsciiFont8x16);
+  M5.Display.setTextSize(M5.Display.height() / 40.0f);
+  M5.Display.setTextWrap(false);
+
 
   /// ボタンの長押し判定の時間 (初期値500) を変更したい場合はこの関数で設定する。
 // btn.setHoldThresh(300); // 判定時間を300ミリ秒に変更。
 
   /// startWriteにはディスプレイに通信バスを占有させ、描画時間を短縮する効果がある。
-  /// 電子ペーパやOLEDモデルの場合、dsp.display(); を呼出すまで通信が保留される。
+  /// 電子ペーパやOLEDモデルの場合、M5.Display.display(); を呼出すまで通信が保留される。
   /// ※ 本来は endWriteと対で使う関数だが、本サンプルでは常時占有したままとする。
-  dsp.startWrite();
+  M5.Display.startWrite();
 }
+
+/// ユーザーの操作内容を表す定数
+enum input_t
+{
+  input_none,
+  input_next,     // 曲送り
+  input_prev,     // 曲戻り
+  input_vol_up,   // ボリューム増加
+  input_vol_down, // ボリューム低下
+};
+
+/// ユーザの操作内容を取得する自作関数
+input_t getInput(void)
+{
+  /// ボタンAしかないパターンを想定
+
+  if (M5.BtnA.wasSingleClicked() || M5.BtnPWR.wasSingleClicked())
+  { /// 1回クリックで曲送り
+    return input_next;
+  }
+
+  if (M5.BtnA.wasDoubleClicked() || M5.BtnPWR.wasDoubleClicked())
+  { /// 2回クリックで曲戻り
+    return input_prev;
+  }
+
+  if (M5.BtnA.isHolding())
+  { /// 長押し時はボリューム操作
+    if (M5.BtnA.getClickCount())
+    { // 長押し前にボタンクリック回数がある場合はボリュームを下げる
+      return input_vol_down;
+    }
+    return input_vol_up;
+  }
+  return input_none;
+}
+
+/// 曲の数 (20個あるという想定)
+static constexpr int data_count = 20;
+
+/// 現在の曲番号
+static int data_index = 0;
+
+/// 現在のボリューム
+static int volume = 128;
 
 void loop(void)
 {
-  delay(1);
+  delay(10);
 
   /// ボタンの状態を更新する。
   M5.update();
 
-  /// 1回クリック時
-  if (btn.wasSingleClicked())
+  switch (getInput())
   {
-    M5_LOGI("wasSingleClicked");
-
-    /// 音を鳴らす
-    spk.tone(500, 500, 0);
-
-    /// 画面の色を黄色にする
-    dsp.fillScreen(TFT_YELLOW);
-  }
-
-  /// 2回クリック時
-  if (btn.wasDoubleClicked())
-  {
-    M5_LOGI("wasDoubleClicked");
-
-    spk.tone(1500, 500, 0);
-    dsp.fillScreen(TFT_BLUE);
-  }
-
-  /// 長押し状態になった時
-  if (btn.wasHold())
-  {
-    M5_LOGI("wasHold   getClickCount:%d", btn.getClickCount());
-
-    dsp.fillScreen((btn.getClickCount() & 1) ? TFT_BLACK : TFT_WHITE);
-  }
-
-  /// 長押し中
-  if (btn.isHolding())
-  {
-    static int Hz = 1000;
-
-    int click_count = btn.getClickCount();
-    /// 長押し直前の単押しの連続回数によって分岐
-    if (click_count & 1) // 回数が奇数の場合
-    {
-      if (Hz > 10) { Hz -= (click_count + 2) >> 1; }  /// 音程を下げる
+  case input_next:
+    if (++data_index >= data_count) {
+      data_index = 0;
     }
-    else /// 回数が偶数の場合
-    {
-      if (Hz < 10000) { Hz += (click_count + 2) >> 1;} /// 音程を上げる
+    break;
+
+  case input_prev:
+    if (--data_index < 0) {
+      data_index = data_count - 1;
     }
-    spk.tone(Hz, 500, 0);
+    break;
+
+  case input_vol_up:
+    if (volume < 255) ++volume;
+    break;
+
+  case input_vol_down:
+    if (volume > 0) --volume;
+    break;
   }
 
-  /// ディスプレイが描画待ちになっているか判定する
-  /// (電子ペーパの場合ここで描画をスキップすることがある)
-  if (!dsp.displayBusy())
   {
-    /// 電子ペーパやUnitOLEDの場合はこのタイミングで画面に反映される。
-    dsp.display();
+    /// 前回のボリューム
+    static int prev_volume = -1;
+
+    /// 前回の曲番号
+    static int prev_index = -1;
+
+    if (prev_volume != volume)
+    { /// ボリュームに変更があった場合の処理
+      M5_LOGI("vl:%d", volume);
+      M5.Display.setCursor(0, 0);
+      M5.Display.printf("vl:%d  ", volume);
+      prev_volume = volume;
+    }
+
+    if (prev_index != data_index)
+    { /// 曲番号に変更があった場合の処理
+      M5_LOGI("No:%d", data_index);
+      M5.Display.setCursor(0, M5.Display.height() - M5.Display.fontHeight());
+      M5.Display.printf("No:%d  ", data_index);
+      prev_index = data_index;
+    }
+  }
+
+  if (!M5.Display.displayBusy())
+  { /// 電子ペーパやOLEDの画面更新処理
+    M5.Display.display();
   }
 }
